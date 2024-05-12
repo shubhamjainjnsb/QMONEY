741d0a316819808cc8c1afae8c344bb63e116137
CrioUser <crio-user@criodo.com>
2024-05-12 19:27:48 +0530

    ME_QMONEY_V2_MODULE_REFACTOR

--- qmoney/src/main/java/com/crio/warmup/stock/portfolio/PortfolioManagerFactory.java
+++ qmoney/src/main/java/com/crio/warmup/stock/portfolio/PortfolioManagerFactory.java
@@ -6,14 +6,15 @@ import org.springframework.web.client.RestTemplate;
 public class PortfolioManagerFactory {
 
   // TODO: CRIO_TASK_MODULE_REFACTOR
-  // Implement the method to return new instance of PortfolioManager.
-  // Remember, pass along the RestTemplate argument that is provided to the new
-  // instance.
+  //  Implement the method to return new instance of PortfolioManager.
+  //  Remember, pass along the RestTemplate argument that is provided to the new instance.
 
-  @Deprecated
   public static PortfolioManager getPortfolioManager(RestTemplate restTemplate) {
+
     return new PortfolioManagerImpl(restTemplate);
-    
   }
+
+
+
 
 }
